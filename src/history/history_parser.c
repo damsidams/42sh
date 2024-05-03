4ba60845717d13827b78455ff93582b45a4543a2
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-05-03 16:34:16 +0200

    [ADD] function to get diffent type of history calls

---
+++ src/history/history_parser.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** 42SH
+** File description:
+** File to start good history functions
+*/
+
+static bool history_sign(char const *str)
+{
+    for (unsigned int i = 0; str[i]; i++) {
+	if (str[i] == HISTORY_CHAR) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+char *check_if_historic(char *cmd)
+{
+    if (!history_sign(cmd)) {
+	return cmd;
+    }
+}

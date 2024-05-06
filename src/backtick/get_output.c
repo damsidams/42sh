77d7953f829ca2f7c5cef1eea51ed8acc845a946
lisa <lisa.goulmot@epitech.eu>
2024-05-06 17:07:55 +0200

    [FIX] initialisation of the alias list and add the prototype in struct.h

--- src/backtick/get_output.c
+++ src/backtick/get_output.c
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2024
-** 42sh
+** 42SH
 ** File description:
-** Function related to backtick
+** Function to get the output of the backtick function
 */
 
 #include <string.h>
@@ -83,7 +83,7 @@ static void clear_func(int *fd, char *old_cmd)
     free(old_cmd);
 }
 
-static char *get_backtick_output(shell_info_t *shell_info, char *cmd)
+char *get_backtick_output(shell_info_t *shell_info, char *cmd)
 {
     char *cmd_result = NULL;
     int output = dup(STDOUT_FILENO);
@@ -104,46 +104,4 @@ static char *get_backtick_output(shell_info_t *shell_info, char *cmd)
     waitpid(pid, NULL, 0);
     clear_func(fd, cmd);
     return cmd_result;
-}
-
-static bool backtick_str(char const *str)
-{
-    if (str == NULL) {
-        return false;
-    }
-    if (strlen(str) <= 1) {
-        return false;
-    }
-    if (str[0] == '`' && str[strlen(str) - 1] == '`') {
-        return true;
-    }
-    return false;
-}
-
-static char *remove_backtick(char *str)
-{
-    char *new_str = calloc(strlen(str) - 1, sizeof(char));
-    unsigned int index = 0;
-
-    if (new_str == NULL) {
-        perror("remove_backtick calloc failed");
-        return NULL;
-    }
-    for (unsigned int i = 1; str[i + 1]; i++) {
-        new_str[i - 1] = str[i];
-        index = i;
-    }
-    new_str[index] = '\0';
-    free(str);
-    return new_str;
-}
-
-void replace_backtick(char **str, shell_info_t *my_shell)
-{
-    for (unsigned int i = 0; str[i]; i++) {
-        if (backtick_str(str[i])) {
-            str[i] = remove_backtick(str[i]);
-            str[i] = get_backtick_output(my_shell, str[i]);
-        }
-    }
 }

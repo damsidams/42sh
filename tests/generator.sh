07b0443da6926486cc5bb9c726600ccb04763343
nicolasnny <nicolas.nunney@gmail.com>
2024-05-08 12:26:01 +0200

    [FIX] -h for the generator and now redirecting all the outputs

--- tests/generator.sh
+++ tests/generator.sh
@@ -1,17 +1,21 @@
 #bin/bash
 
 function generate_test() {
-    if [ "$1" = "-h" ]; then
-        echo "generate_test n t_name cmd"
-        echo "n         test num"
-        echo "t_nname   test name"
-        echo "cmd       test cmd"
+    if [ $1 = "-h" ]; then
+        echo "
+        add a new test for your 42sh project:
+        you will need 4 arguments:
+            - 1: the number of your test (ex: 1, 2, 3, 4)
+            - 2: the name of your test (ex: "simple ls command")
+            - 3: the command you want to execute (ex: "ls")
+            - 4: the file you want to write your test in
+        "
     else
         echo "
         ##$2
         mkdir tnum/$1 -p
-        echo \"$3\" | ./42sh > tnum/$1/output
-        echo \"$3\" | tcsh  > tnum/$1/desired
+        echo \"$3\" | ./42sh &> tnum/$1/output
+        echo \"$3\" | tcsh  &> tnum/$1/desired
         if diff -q tnum/$1/desired tnum/$1/output; then
             echo -e \"\e[32m[TEST $1] $2 Test PASSED\e[0m\"
         else

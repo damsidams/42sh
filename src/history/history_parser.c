5aaa8ee1bf5225fc9a2b8a9df59205415bb13680
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-05-10 12:45:49 +0200

    [FIX] historic: change command if hist char is called - check that the command is not only ! - get the adress to replace the user input

--- src/history/history_parser.c
+++ src/history/history_parser.c
@@ -69,7 +69,7 @@ static char *get_good_string(char *cmd,
 
 static bool history_sign(char const *str)
 {
-    if (str == NULL) {
+    if (str == NULL || strlen(str) <= 1) {
         return false;
     }
     for (unsigned int i = 0; str[i]; i++) {
@@ -113,21 +113,25 @@ static char *parse_cmd(char *cmd, bool *change)
     return cmd;
 }
 
-char *check_if_historic(char *cmd, shell_info_t *my_shell)
+char *check_if_historic(char **cmd, shell_info_t *my_shell)
 {
     bool change = false;
+    char *new = NULL;
 
-    if (!history_sign(cmd)) {
-        return cmd;
+    if (!history_sign(*cmd)) {
+        return *cmd;
     }
-    cmd = parse_cmd(cmd, &change);
-    if (cmd == NULL) {
+    new = parse_cmd(*cmd, &change);
+    if (new == NULL) {
+        free(*cmd);
         my_shell->exit_status = 1;
         return NULL;
     }
     my_shell->exit_status = 0;
     if (change) {
-        printf("%s\n", cmd);
+        printf("%s\n", new);
+        free(*cmd);
+        *cmd = new;
     }
-    return cmd;
+    return *cmd;
 }
